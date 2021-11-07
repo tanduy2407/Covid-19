17c581b57ed9b6f49ab40c25c647e53157367fd5
TANDUY <phamduy240797@gmail.com>
2021-11-07 09:20:38 +0700

    Update vaccine code and delete csv file

--- vaccination_data.py
+++ vaccination_data.py
@@ -14,22 +14,18 @@ def get_vaccine_data():
 		kaggle.api.dataset_download_files(
 			'gpreda/covid-world-vaccination-progress', path='D:/PycharmProjects/Python/Covid-19/data', unzip=True)
 		print('Download files successfully')
+		file_path = fr'{WORKING_PATH}\data\country_vaccinations.csv'
+		df = pd.read_csv(file_path)
+		use_cols = ['country', 'date', 'total_vaccinations', 'people_vaccinated',
+					'people_fully_vaccinated', 'daily_vaccinations', 'vaccines']
+		final_df = df[use_cols]
+		final_df['date'] = pd.to_datetime(df['date'])
+		final_df = final_df.replace({np.NAN: None})
+		final_df['run_date'] = date.today().strftime('%Y-%m-%d')
+		print(final_df[final_df['country']=='Vietnam'])
 	except Exception as err:
 		print(err)
 
 
-def load_vaccine():
-	file_path = fr'{WORKING_PATH}\data\country_vaccinations.csv'
-	df = pd.read_csv(file_path)
-	use_cols = ['country', 'date', 'total_vaccinations', 'people_vaccinated',
-				'people_fully_vaccinated', 'daily_vaccinations', 'vaccines']
-	final_df = df[use_cols]
-	final_df['date'] = pd.to_datetime(df['date'])
-	final_df = final_df.replace({np.NAN: None})
-	final_df['run_date'] = date.today().strftime('%Y-%m-%d')
-	print(final_df[final_df['country']=='Vietnam'])
-	
-
-
 get_vaccine_data()
-load_vaccine()
+
