6da7c67e6e50a847251886f6b09aa51fb23ec026
TANDUY <phamduy240797@gmail.com>
2021-11-14 15:51:15 +0700

    Update code

--- connect_mysql.py
+++ connect_mysql.py
@@ -1,6 +1,7 @@
 import MySQLdb
 import pandas as pd
 
+
 def connectDB():
 	try:
 		connection = MySQLdb.connect(
@@ -18,6 +19,33 @@ def connectDB():
 
 
 conn, cursor = connectDB()
+
+
+def query(keyw, table_name, num_cols=None, data_arr=None):
+	if keyw == 'Read':
+		sql = f'SELECT * FROM covid_data.{table_name}'
+		df = pd.read_sql_query(sql, conn)
+		return df
+
+	if keyw == 'Insert':
+		bind = ', '.join(["%s" for _ in range(num_cols)])
+		sql = f"INSERT INTO covid_data.{table_name} VALUES ({bind})"
+		try:
+			cursor.executemany(sql, data_arr)
+			conn.commit()
+			print(f'Insert data {len(data_arr)} rows to {table_name}')
+		except Exception as err:
+			print(err)
+
+	if keyw == 'Truncate':
+		sql = f'TRUNCATE TABLE covid_data.{table_name}'
+		try:
+			cursor.execute(sql)
+			print(f'Truncate table {table_name}')
+		except Exception as err:
+			print(err)
+
+
 def create_summary_table():
 	try:
 		sql = """CREATE TABLE covid_data.SUMMARY_COVID_DATA
@@ -43,27 +71,40 @@ def create_summary_table():
 	except Exception as err:
 		print(err)
 
+# create_summary_table()
+def create_vaccine_table():
+	try:
+		sql = """CREATE TABLE covid_data.VACCINE_COVID_DATA
+					(COUNTRY VARCHAR(100),
+					ISO_CODE VARCHAR(10),
+					DATE DATE,
+					TOTAL_VACCINATIONS BIGINT,
+					PEOPLE_VACCINATED BIGINT,
+					PEOPLE_FULLY_VACCINATED BIGINT,
+					DAILY_VACCINATIONS INT,
+					TOTAL_VACCINATIONS_PER_HUNDRED FLOAT,
+					PEOPLE_VACCINATED_PER_HUNDRED FLOAT,
+					PEOPLE_FULLY_VACCINATED_PER_HUNDRED FLOAT,
+					DAILY_VACCINATIONS_PER_MILLION FLOAT,
+					VACCINES VARCHAR(300))"""
+		cursor.execute(sql)
+		print('Create table success')
+	except Exception as err:
+		print(err)
 
-def query(keyw, table_name, num_cols=None, data_arr=None):
-	if keyw == 'Read':
-		sql = f'SELECT * FROM covid_data.{table_name}'
-		df = pd.read_sql_query(sql, conn)
-		return df
+# create_vaccine_table()
 
-	if keyw == 'Insert':
-		bind = ', '.join(["%s" for _ in range(num_cols)])
-		sql = f"INSERT INTO covid_data.{table_name} VALUES ({bind})"
-		try:
-			cursor.executemany(sql, data_arr)
-			conn.commit()
-			print(f'Insert data {len(data_arr)} rows to {table_name}')
-		except Exception as err:
-			print(err)
-	
-	if keyw == 'Truncate':
-		sql = f'TRUNCATE TABLE covid_data.{table_name}'
-		try:
-			cursor.execute(sql)
-			print(f'Truncate table {table_name}')
-		except Exception as err:
-			print(err)
+def create_detail_table():
+	try:
+		sql = """CREATE TABLE covid_data.DETAIL_COVID_DATA(
+				COUNTRY VARCHAR(100),
+				DATE DATE,
+				TOTAL_CONFIRMED BIGINT,
+				TOTAL_DEATHS BIGINT,
+				MORTALITY_RATE FLOAT)"""
+		cursor.execute(sql)
+		print('Create table success')
+	except Exception as err:
+		print(err)
+
+create_detail_table()
--- crawl_vaccine.py
+++ crawl_vaccine.py
@@ -3,6 +3,8 @@ import os
 import pandas as pd
 import numpy as np
 from datetime import date
+from connect_mysql import query
+
 
 WORKING_PATH = os.path.dirname(os.path.realpath(__file__))
 
@@ -11,7 +13,7 @@ def get_vaccine_data():
 	try:
 		kaggle.api.authenticate()
 		kaggle.api.dataset_download_files(
-			'gpreda/covid-world-vaccination-progress', path='D:/PycharmProjects/Python/Covid-19/data', unzip=True)
+			'gpreda/covid-world-vaccination-progress', path=f'{WORKING_PATH}/data', unzip=True)
 		print('Download files successfully')
 		file_path = fr'{WORKING_PATH}\data\country_vaccinations.csv'
 		df = pd.read_csv(file_path)
@@ -26,9 +28,9 @@ def get_vaccine_data():
 
 # get_full_vaccine_data()
 def get_full_vaccines():
-	df_with_vaccine = pd.read_csv(r'D:\PycharmProjects\Python\Covid-19\data\country_vaccinations.csv', usecols=['country', 'vaccines'])
+	df_with_vaccine = pd.read_csv(rf'{WORKING_PATH}\data\country_vaccinations.csv', usecols=['country', 'vaccines'])
 	df_with_vaccine = df_with_vaccine.groupby(by='country').max()
-	df_with_vaccine.to_csv(r'D:\PycharmProjects\Python\Covid-19\data\country_with_vaccines.csv', index=False)
+	df_with_vaccine.to_csv(rf'{WORKING_PATH}\data\country_with_vaccines.csv', index=False)
 
 	df_no_vaccine = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv')
 	df_no_vaccine['vaccines'] = ''
@@ -42,10 +44,13 @@ def get_full_vaccines():
 	df_no_vaccine = df_no_vaccine.reset_index()
 	final_df_vaccine = df_no_vaccine[df_no_vaccine['vaccines'] != '']
 	final_df_vaccine.rename(columns = {'location':'country'}, inplace = True)
-	final_df_vaccine = final_df_vaccine.drop(['total_boosters', 'total_boosters_per_hundred'], axis=1)
+	final_df_vaccine = final_df_vaccine.drop(['daily_vaccinations_raw', 'total_boosters', 'total_boosters_per_hundred'], axis=1)
 	final_df_vaccine = final_df_vaccine.replace({np.NaN:None})
-	print(final_df_vaccine)
-	final_df_vaccine.to_csv(r'D:\PycharmProjects\Python\Covid-19\data\full_vaccinations.csv', index=False)
+	### update daily vaccine data
+	vaccine_data = [tuple(row) for row in final_df_vaccine.values]
+	query(keyw='Truncate', table_name='VACCINE_COVID_DATA')
+	query(keyw='Insert', table_name='VACCINE_COVID_DATA', num_cols=12, data_arr=vaccine_data)
+	# final_df_vaccine.to_csv(rf'{WORKING_PATH}\data\full_vaccinations.csv', index=False)
 
-get_vaccine_data()
+# get_vaccine_data()
 get_full_vaccines()
