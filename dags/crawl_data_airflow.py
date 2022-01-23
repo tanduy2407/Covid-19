16f50a62054402bb3ad83b3d99345b692ec2643b
TANDUY <phamduy240797@gmail.com>
2022-01-23 15:47:30 +0700

    Update code

--- dags/crawl_data_airflow.py
+++ dags/crawl_data_airflow.py
@@ -22,29 +22,33 @@ def get_summary_data():
 		headers = {
 			'x-rapidapi-host': "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com",
 			'x-rapidapi-key': "282e773d97msh20602f20182196ep1bbea3jsn8b10ac27c324"}
-		response = requests.request("GET", url, headers=headers).json()
-		for country in response:
-			rank = country['rank']
-			name = country['Country']
-			continent = country['Continent']
-			infection_risk = country['Infection_Risk']
-			serious_critical = country['Serious_Critical']
-			active_cases = country['ActiveCases']
-			fatality_rate = country['Case_Fatality_Rate']
-			test_percentage = country['Test_Percentage']
-			recovery_proporation = country['Recovery_Proporation']
-			total_cases = country['TotalCases']
-			new_cases = country['NewCases']
-			total_deaths = country['TotalDeaths']
-			new_deaths = country['NewDeaths']
-			total_recovered = country['TotalRecovered']
-			total_tests = country['TotalTests']
-			population = country['Population']
-			run_date = date.today().strftime('%Y-%m-%d')
-			info = (rank, name, continent, infection_risk, serious_critical, fatality_rate, active_cases, total_cases,
-					new_cases, total_deaths, new_deaths, total_recovered, recovery_proporation, total_tests, test_percentage, population, run_date)
-			sum_data.append(info)
-	print('Crawl summary data successfully')
+		try:
+			response = requests.request("GET", url, headers=headers).json()
+			for country in response:
+				rank = country['rank']
+				name = country['Country']
+				continent = country['Continent']
+				infection_risk = country['Infection_Risk']
+				serious_critical = country['Serious_Critical']
+				active_cases = country['ActiveCases']
+				fatality_rate = country['Case_Fatality_Rate']
+				test_percentage = country['Test_Percentage']
+				recovery_proporation = country['Recovery_Proporation']
+				total_cases = country['TotalCases']
+				new_cases = country['NewCases']
+				total_deaths = country['TotalDeaths']
+				new_deaths = country['NewDeaths']
+				total_recovered = country['TotalRecovered']
+				total_tests = country['TotalTests']
+				population = country['Population']
+				run_date = date.today().strftime('%Y-%m-%d')
+				info = (rank, name, continent, infection_risk, serious_critical, fatality_rate, active_cases, total_cases,
+						new_cases, total_deaths, new_deaths, total_recovered, recovery_proporation, total_tests, test_percentage, population, run_date)
+				sum_data.append(info)
+			print('Crawl summary data successfully')
+		except Exception as err:
+			print('Error:', err)
+			break
 	query(keyw='Truncate', table_name='SUMMARY_COVID_DATA')
 	query(keyw='Insert', table_name='SUMMARY_COVID_DATA', num_cols=17, data_arr=sum_data)
 
